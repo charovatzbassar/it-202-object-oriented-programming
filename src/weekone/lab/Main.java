1a937376b1ccfbe4c1433d4d6995e3d543f8c9da
Basar Carovac <carovac.basar@gmail.com>
2023-10-05 13:18:08 +0200

    add task numbers

--- src/weekone/lab/Main.java
+++ src/weekone/lab/Main.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //1
         System.out.println("Basar Carovac");
 
+        //2
         System.out.println("Hello world!");
         System.out.println("(and all the people of the world)");
 
+        //3
         System.out.println("There are " + (365*24*60*60) + " seconds in a year");
 
+        //4
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Type a number: ");
         int firstNumber = scanner.nextInt();
@@ -21,10 +25,12 @@ public class Main {
 
         System.out.println("Sum of the numbers: " + (firstNumber + secondNumber));
 
+        //5
         System.out.printf("Type a number: ");
         int number = scanner.nextInt();
         System.out.println(number > 0 ? "Number is positive" : "Number is not positive");
 
+        //6
         System.out.printf("Type the first number: ");
         int firstNum = scanner.nextInt();
 
