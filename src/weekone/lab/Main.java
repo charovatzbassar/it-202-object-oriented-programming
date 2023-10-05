635bf47e11d5b4e667dd479773e1c1069a7aec1d
Basar Carovac <carovac.basar@gmail.com>
2023-10-05 13:15:37 +0200

    add lab problems

--- src/weekone/lab/Main.java
+++ src/weekone/lab/Main.java
@@ -1,7 +1,44 @@
 package weekone.lab;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Basar Carovac");
+
+        System.out.println("Hello world!");
+        System.out.println("(and all the people of the world)");
+
+        System.out.println("There are " + (365*24*60*60) + " seconds in a year");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Type a number: ");
+        int firstNumber = scanner.nextInt();
+
+
+        System.out.printf("Type another number: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Sum of the numbers: " + (firstNumber + secondNumber));
+
+        System.out.printf("Type a number: ");
+        int number = scanner.nextInt();
+        System.out.println(number > 0 ? "Number is positive" : "Number is not positive");
+
+        System.out.printf("Type the first number: ");
+        int firstNum = scanner.nextInt();
+
+
+        System.out.printf("Type the second number: ");
+        int secondNum = scanner.nextInt();
+
+        if (firstNum == secondNum) {
+            System.out.println("They are equal");
+        } else {
+            int greaterNum = firstNum > secondNum ? firstNum : secondNum;
+            System.out.println("Greater number: " + greaterNum);
+        }
+
+
     }
 }
