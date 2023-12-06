b9af52a79fbbf64266797c09ada89a10662e1514
charovatzbassar <carovac.basar@gmail.com>
2023-12-06 13:21:44 +0100

    problem 1 classes

---
+++ src/main/java/week10/lab/Admin.java
@@ -0,0 +1,17 @@
+package week10.lab;
+
+public class Admin {
+    private String username;
+
+    public Admin(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/week10/lab/User.java
@@ -0,0 +1,17 @@
+package week10.lab;
+
+public class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
