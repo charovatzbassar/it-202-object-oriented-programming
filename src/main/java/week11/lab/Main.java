5c4ac5c2f3b415aeb1375fce0fb44ccc8e690d92
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-12-16 16:08:28 +0100

    exception problem

--- src/main/java/week11/lab/Main.java
+++ src/main/java/week11/lab/Main.java
@@ -1,4 +1,17 @@
 package week11.lab;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+
+        StudentSystem sis = new StudentSystem("src/main/java/week11/lab/students.csv");
+        List<Student> students = sis.readStudents();
+        System.out.println(sis.getStudentById(10));
+        System.out.println(sis.getHighestGPAStudent());
+        System.out.println(sis.getLongestNameStudent());
+    }
 }
